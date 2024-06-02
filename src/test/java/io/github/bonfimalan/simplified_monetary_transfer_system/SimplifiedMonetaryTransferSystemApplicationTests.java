24f4e88fcf7c8d7ad0621208cf9b30df7588a1a5
bonfimalan <alanbonfim2000@gmail.com>
2024-06-01 21:07:08 -0300

    Initial project

---
+++ src/test/java/io/github/bonfimalan/simplified_monetary_transfer_system/SimplifiedMonetaryTransferSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bonfimalan.simplified_monetary_transfer_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifiedMonetaryTransferSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
